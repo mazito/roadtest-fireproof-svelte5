fc6dca43b7eb022f452afa3ed15d9b62ee5f0a98
Mario Zito <mario.zito@treme.io>
2024-12-22 23:22:12 -0300

    Test open ledger

---
+++ src/routes/+layout.ts
@@ -0,0 +1,8 @@
+import "../app.css";
+
+//Desactivar Prerrenderización
+/*Se debe deshabilitar el enrutamiento y el prerrendamiento del lado del servidor,
+ así como habilitar el enrutamiento del lado del cliente */
+export const ssr = false;
+export const csr = true;
+export const prerender = false;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { fireproof } from "use-fireproof";
+
+  let 
+    ledger: any,
+    error: any,
+    name = '';
+
+  function openLedger() {
+    try {
+      ledger = fireproof(name);
+    }
+    catch (err: any) {
+      error = err;
+    }
+  }
+
+</script>
+
+
+
+<div class="p-8">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+  <hr class="my-2">
+  <input class="p-2 border w-32" bind:value={name} /> 
+  <button class="p-2 bg-gray-100 border" on:click={openLedger}>Open ledger</button>
+  <div class="p-2">Ledger: <pre>{JSON.stringify(ledger,null,4)}</pre></div>
+  <div class="p-2">Error: {error}</div>
+
+  <hr class="my-2">
+
+</div>
